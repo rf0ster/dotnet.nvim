43a16a4372715b0099f613116947e551d421edbe
Riley Foster <rileyfoster@Rileys-MacBook-Pro.local>
2025-03-02 08:19:03 -0600

    Cleanup copying of config options.

--- lua/dotnet/config.lua
+++ lua/dotnet/config.lua
@@ -1,9 +1,12 @@
-local M = {}
+local M = {
+    opts = {}
+}
 
 M.setup = function(opts)
-    for k, v in pairs(opts) do
-        M[k] = v
-    end
+    opts = opts or {}
+    M.opts = vim.tbl_deep_extend("force", {}, M.opts, opts)
+
+    print(vim.inspect(M))
 end
 
 return M
